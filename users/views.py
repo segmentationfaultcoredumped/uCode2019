16442b54ec5d73554635bad44fa98033c42be540
Joan Palau <joanp4l4u@gmail.com>
2019-03-23 16:06:12 +0100

    Added users and rerouted urls

--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+
+@login_required
+def profile(request):
+    context = {
+        'title': request.user.name,
+    }
+    return render(request, 'users/profile.html', context)
+
+
+def inici(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        return redirect('login')
--- workflow/urls.py
+++ workflow/urls.py
@@ -17,5 +17,5 @@ Including another URLconf
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('data.urls')),
+    path('', include('users.urls')),
 ]
