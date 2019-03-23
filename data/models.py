569edfbb625e7dd837184546afbb0cae5ba389d9
Joan Palau <joanp4l4u@gmail.com>
2019-03-23 08:05:20 +0100

    Creades vistes basiques

--- data/models.py
+++ data/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+
+
+class Session(models.Model):
+    BOOL_CHOICES = ((True, 'Natural'), (False, 'Artificial'))
+
+    # General info
+    name = models.CharField(max_length=64)
+    training = models.TextField(max_length=500, blank=True)
+    date = models.DateField(default=datetime.date.today)
+    # Environment conditions
+    grass = models.BooleanField(choices=BOOL_CHOICES, blank=True)
+    wet = models.BooleanField(blank=True)
+    temp = models.IntegerField(blank=True)
+    hum = models.IntegerField(blank=True)
+    additional_info = models.TextField(max_length=240, blank=True)
--- data/urls.py
+++ data/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from .views import SessionsView
+from .views import SessionView
 
 urlpatterns = [
     # path('', admin.site.urls, 'home'),
+    path('home/', SessionsView.as_view(), 'home'),
+    path('home/sessions/<int:pk>', SessionView.as_view(),'detail-session')
 ]
