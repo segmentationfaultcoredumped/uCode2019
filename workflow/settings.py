3c2043d9bdea9a987a1730fe18b270a2e83effc2
Joan Palau <joanp4l4u@gmail.com>
2019-03-23 23:22:43 +0100

    Tools Initial Commit

--- workflow/settings.py
+++ workflow/settings.py
@@ -85,6 +85,16 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'HOST': 'db',
+#         'PORT': 5432,
+#     }
+# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
